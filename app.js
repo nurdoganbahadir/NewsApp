c51745ef0b019d8271350b687faea58d423d25d2
Nurdoğan Bahadır <nurdoganbahadirr@gmail.com>
2024-08-10 18:02:14 +0300

    js update

--- app.js
+++ app.js
@@ -1,5 +1,48 @@
 //*my api key = 2e64332ddcb64dc7b50dd5c1ab45d8a4
+const searchInput = document.querySelector("#searchInput");
+const country = document.querySelector(".form-select");
+document.querySelector(".btn").onclick = () => {
+  const news = async () => {
+    const BASE_URL = "https://newsapi.org/v2/everything";
+    const API_KEY = "2e64332ddcb64dc7b50dd5c1ab45d8a4";
+    const query = searchInput.value;
+    const language = country.options[country.selectedIndex].value;
 
-const news = async () => {
-    const MY_URL
+    try {
+      const URL = `${BASE_URL}?q=${query}&apiKey=${API_KEY}&language=${language}`;
+      const res = await fetch(URL);
+      const data = await res.json();
+      newsScreen(data.articles);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+  news();
+};
+
+function newsScreen(news) {
+  const rowScreen = document.querySelector(".row");
+  rowScreen.innerHTML = "";
+  news.forEach((item) => {
+    const { title, content, urlToImage, url } = item;
+    rowScreen.innerHTML += `
+    <div class="col-sm-12 col-md-6 col-xl-4">
+        <div class="card">
+            <img src="${urlToImage}" class="card-img-top" alt="..." />
+            <div class="card-body">
+              <h5 class="card-title">${title}</h5>
+              <p class="card-text">
+                ${content}
+              </p>
+              <a href="${url}" class="btn btn-primary">Go somewhere</a>
+              <a href=""></a>
+            </div>
+        </div>
+    </div>
+    `;
+  });
+}
+
+document.querySelector("body").onload = function () {
+  searchInput.focus();
 };
